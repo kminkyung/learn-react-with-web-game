b724bbc9408eccb1ad6ddf0bd39b882be8656a55
kminkyung <mminkyung.k@gmail.com>
2020-07-06 21:40:25 +0900

    Install webpack and babel for build React project, configure webpack

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+  name: 'word-chain-setting',
+  mode: 'development', // 실서비스 : 'production'
+  devtool: 'eval',
+  resolve: {
+    extensions: ['.jsx', '.js']
+  },
+  entry: {
+    app: ['./client']
+  },
+  module: {
+    rules: [{
+      test: /\.jsx?/,
+      loader: 'babel-loader',
+      options: {
+        presets: ['@babel/preset-env', '@babel/preset-react'],
+        plugins: ['@babel/plugin-proposal-class-properties']
+      }
+    }]
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: "app.js"
+  },
+}
