e0ff6097e9eb292074ff4b913f3f4c9564637e0c
kminkyung <mminkyung.k@gmail.com>
2020-07-08 21:25:23 +0900

    Complete number-baseball

--- number-baseball/NumberBaseball.jsx
+++ number-baseball/NumberBaseball.jsx
@@ -1,45 +1,87 @@
 const React = require('react');
-const { Component } = React;
+const {Component} = React;
 const Try = require('./Try');
 
 function getNumbers() {
-
+  const candidate = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  const arr = [...Array(4)];
+  return arr.map((v, i) => candidate.splice(Math.floor(Math.random() * (9 - i)), 1)[0]);
 }
 
 class NumberBaseball extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      result: '',
+      result: '숫자야구',
       value: '',
       answer: getNumbers(),
       tries: []
     }
+    // this.onChangeInput().this.bind(this); 화살표 함수를 쓰지 않으려면 넣어주어야 함
   }
-  onChangeInput = () => {
 
+  onChangeInput = (e) => {
+    this.setState({
+      value: e.target.value
+    })
   }
-  onSubmitForm = () => {
 
+  onSubmitForm = (e) => {
+    e.preventDefault();
+    if (this.state.value === this.state.answer.join('')) { // 정답
+      this.setState({
+        result: 'Home run!',
+        tries: [...this.state.tries, {try: this.state.value, result: 'Home run!'}]
+      })
+      alert('게임을 다시 시작합니다.');
+      this.setState({
+        value: '',
+        answer: getNumbers(),
+        tries: []
+      })
+    } else { // 오답
+      const answerArray = this.state.value.split('').map(v => parseInt(v));
+      let strike = 0;
+      let ball = 0;
+
+      if (this.state.tries.length >= 10) { // 오답 10번
+        this.setState({
+          result: `10번 넘게 틀려서 실패! 답은 ${this.answer.join('')}였습니다!`
+        })
+        alert('성공! 게임을 다시 시작합니다.');
+        this.setState({
+          value: '',
+          answer: getNumbers(),
+          tries: []
+        })
+      } else { // 다시 기회
+        this.state.answer.forEach((answer, i) => {
+          if (answerArray[i] === answer) {
+            strike++;
+          } else if (answerArray.includes(answer)) {
+            ball++;
+          }
+        })
+        this.setState({
+          value: '',
+          tries: [...this.state.tries, {try: this.state.value, result : ` : ${strike} 스트라이크, ${ball} 볼입니다.`}]
+        })
+      }
+    }
   }
-  fruit = [
-    ['사과', '맛있다'],
-    ['바나나', '맛없다'],
-    ['배', '시다'],
-    ['귤', '시다'],
-    ['감', '떫다'],
-    ['포도', '시다']
-  ];
-  render() {
+
+
+  render() { // render는 Component가 처리해주므로 화살표 함수 쓸 필요 없음
     return (
       <>
         <h1>{this.state.result}</h1>
         <form onSubmit={this.onSubmitForm}>
           <input maxLength={4} value={this.state.value} onChange={this.onChangeInput}/>
+          <button type='submit'>시도</button>
         </form>
         <div>시도 : {this.state.tries.length}</div>
         <ul>
-          {this.fruit.map((v, i) => < Try value={v} index={i}/>)}
+          {this.state.tries.map((v, i) => < Try key={i} tryInfo={v} />)}
         </ul>
       </>
     )
--- number-baseball/Try.jsx
+++ number-baseball/Try.jsx
@@ -2,13 +2,11 @@ const React = require('react');
 const { Component } = React;
 
 class Try extends Component {
-  render() {
+  render(props) {
     return (
-      <li key={this.props.index}>
-        <b>{this.props.value[0]}</b> - {this.props.value[1]}
-        <div>Content</div>
-        <div>Content</div>
-        <div>Content</div>
+      <li>
+        <b>{this.props.tryInfo.try}</b>
+        <b>{this.props.tryInfo.result}</b>
       </li>
     )
   }
