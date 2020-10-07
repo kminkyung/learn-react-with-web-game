155f34edee0b316463067bdb78578b557c57588d
kminkyung <mminkyung.k@gmail.com>
2020-10-07 19:59:06 +0900

    TO HARD FOR ME

--- mine-search/MineSearch.jsx
+++ mine-search/MineSearch.jsx
@@ -15,6 +15,7 @@ export const CODE = {
 
 export const TableContext = createContext({
   tableData : [],
+  halted: true,
   dispatch: () => {},
 });
 
@@ -22,6 +23,7 @@ const initialState = {
   tableData: [],
   timer: 0,
   result: '',
+  halted: true
 }
 
 // shuffle 정
@@ -55,36 +57,154 @@ const plantMine = (row, cell, mine) => {
 
 export const START_GAME = 'START_GAME';
 export const OPEN_CELL = 'OPEN_CELL';
+export const CLICK_MINE = 'CLICK_MINE';
+export const FLAG_CELL = 'FLAG_CELL';
+export const QUESTION_CELL = 'QUESTION_CELL';
+export const NORMALIZE_CELL = 'NORMALIZE_CELL';
 
 const reducer = (state, action) => {
   switch (action.type) {
     case START_GAME:
       return {
         ...state,
-        tableData: plantMine(action.row, action.cell, action.mine)
+        tableData: plantMine(action.row, action.cell, action.mine),
+        halted: false,
       }
-    case OPEN_CELL:
+    case OPEN_CELL: {
       const tableData = [...state.tableData];
-      tableData[action.row] = [state.tableData[action.row]];
-      tableData[action.row][action.cell] = CODE.OPENED;
+      // 주변 칸들을 열어야하기 때문에 모든 칸들을 새로운 객체로 만듬
+      tableData.forEach((row, i) => {
+        tableData[i] = [...state.tableData[i]];
+      })
+      const checked = []; // 한번 검사한 칸은 다시 검사하지 않도록 캐싱
+      // 자기 기준으로 검사하는 함수
+      const checkAround = (row, cell) => {
+        if(row < 0 || row >= tableData.length || cell < 0 || cell >= tableData[0].length) { // 상하좌우칸이 아닌 경우 필터링
+          return;
+        }
+        if ([CODE.OPENED, CODE.FLAG_MINE, CODE.FLAG, CODE.QUESTION_MINE, CODE.QUESTION].includes(tableData[row][cell])) {
+          return;
+        }
+        if(checked.includes(row + "," + cell)) { // 이미 검사한 칸이면
+          return;
+        } else { // 한번 연 칸 무시
+          checked.push(row + ',' + cell);
+        }
+        let around = [];
+        if(tableData[row - 1]) {
+          around = around.concat(
+            tableData[row-1][cell-1],
+            tableData[row-1][cell],
+            tableData[row-1][cell+1]
+          )
+        }
+        around = around.concat(
+          tableData[row][cell-1],
+          tableData[row][cell+1],
+        )
+        if(tableData[row + 1]) {
+          around = around.concat(
+            tableData[row+1][cell-1],
+            tableData[row+1][cell],
+            tableData[row+1][cell+1]
+          )
+        }
+        const count = around.filter(v => [CODE.MINE, CODE.FLAG_MINE, CODE.QUESTION_MINE].includes(v)).length;
+        tableData[row][cell] = count;
+        if(count === 0) {
+          const near = [];
+          if(row - 1 > -1) { // 제일 윗칸을 클릭한 경우
+            near.push([row-1, cell-1]);
+            near.push([row-1, cell]);
+            near.push([row-1, cell+1]);
+          }
+          near.push([row, cell-1]);
+          near.push([row, cell+1]);
+          if(row + 1 > tableData.length) { // 제일 아랫칸을 클릭한 경우
+            near.push([row+1, cell-1]);
+            near.push([row+1, cell]);
+            near.push([row+1, cell+1]);
+          }
+          near.forEach((n) => { // 이거뭐
+            if(tableData[n[0]][n[1]] !== CODE.OPENED) {
+              checkAround(n[0], n[1]);
+            }
+          })
+        }
+        tableData[row][cell] = count;
+      }
+
+      checkAround(action.row, action.cell);
+
       return {
         ...state,
         tableData,
       }
+    }
+    case CLICK_MINE: {
+      const tableData = [...state.tableData];
+      tableData[action.row] = [...state.tableData[action.row]];
+      tableData[action.row][action.cell] = CODE.CLICKED_MINE;
+      return {
+        ...state,
+        tableData,
+        halted: true,
+      }
+    }
+    case FLAG_CELL: {
+      const tableData = [...state.tableData];
+      tableData[action.row] = [...state.tableData[action.row]];
+      if(tableData[action.row][action.cell] === CODE.MINE) {
+        tableData[action.row][action.cell] = CODE.FLAG_MINE;
+      } else {
+        tableData[action.row][action.cell] = CODE.FLAG;
+      }
+      return {
+        ...state,
+        tableData,
+      }
+    }
+    case QUESTION_CELL: {
+      const tableData = [...state.tableData];
+      tableData[action.row] = [...state.tableData[action.row]];
+      if(tableData[action.row][action.cell] === CODE.FLAG_MINE) {
+        tableData[action.row][action.cell] = CODE.QUESTION_MINE;
+      } else {
+        tableData[action.row][action.cell] = CODE.QUESTION;
+      }
+      return {
+        ...state,
+        tableData,
+      }
+    }
+    case NORMALIZE_CELL: {
+      const tableData = [...state.tableData];
+      tableData[action.row] = [...state.tableData[action.row]];
+      if(tableData[action.row][action.cell] === CODE.QUESTION_MINE) {
+        tableData[action.row][action.cell] = CODE.MINE;
+      } else {
+        tableData[action.row][action.cell] = CODE.NORMAL;
+      }
+      return {
+        ...state,
+        tableData,
+      }
+    }
     default:
         return state;
   }
 }
 const MineSearch = () => {
   const [state, dispatch] = useReducer(reducer, initialState);
-  const value = useMemo(() => ({ tableData: state.tableData, dispatch }), [state.tableData]);
+  const {tableData, halted, timer, result} = state;
+  const value = useMemo(() => ({ tableData: tableData, halted: halted, dispatch }), [tableData, halted]);
 
   return (
     <TableContext.Provider value={value}>
       <Form />
-      <div>{state.timer}</div>
+      <div>{timer}</div>
       <Table />
-      <div>{state.timer}</div>
+      <div>{timer}</div>
     </TableContext.Provider>
   )
 }
--- mine-search/Td.jsx
+++ mine-search/Td.jsx
@@ -48,13 +48,15 @@ const getTdText = (code) => {
     case CODE.QUESTION:
       return '?';
     default:
-      return '';
+      return code || '';
   }
 }
 
 const Td = ({rowIndex, cellIndex}) => {
-  const {tableData, dispatch} = useContext(TableContext);
-
+  const {tableData, dispatch, halted} = useContext(TableContext);
+  if (halted) {
+    return;
+  }
   const onClickTd = useCallback(() => {
     switch (tableData[rowIndex][cellIndex]) {
       case CODE.OPENED:
@@ -68,12 +70,32 @@ const Td = ({rowIndex, cellIndex}) => {
         return;
       case CODE.MINE:
         dispatch({type: CLICK_MINE, row: rowIndex, cell: cellIndex})
+      default:
+        return;
     }
-  }, [])
+  }, [tableData[rowIndex][cellIndex], halted])
 
-  const onRightClickTd = useCallback(() => {
-
-  }, []);
+  const onRightClickTd = useCallback((e) => {
+    e.preventDefault();
+    if(halted) {
+      return;
+    }
+    switch (tableData[rowIndex][cellIndex]) {
+      case CODE.NORMAL:
+      case CODE.MINE:
+        dispatch({type: FLAG_CELL, row: rowIndex, cell: cellIndex});
+        return;
+      case CODE.FLAG_MINE:
+      case CODE.FLAG:
+        dispatch({type: QUESTION_CELL, row:rowIndex, cell:cellIndex});
+        return;
+      case CODE.QUESTION:
+        dispatch({type: NORMALIZE_CELL, row:rowIndex, cell:cellIndex});
+        return;
+      default:
+        return;
+    }
+  }, [tableData[rowIndex][cellIndex], halted]);
 
   return (
     <td
